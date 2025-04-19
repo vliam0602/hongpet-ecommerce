e42f3e759034c07d65880e52c13098451eb19bb4
vliam0602 <v.trclam@gmail.com>
2025-04-19 23:09:51 +0700

    add jwt

---
+++ HongPet.Backend/src/HongPet.Application/Commons/GenerateJwtString.cs
@@ -0,0 +1,37 @@
+﻿using HongPet.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HongPet.Application.Commons;
+public static class GenerateJwtString
+{
+    public static string GenerateJsonWebToken(this User user, string secretKey, DateTime now)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var credentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier ,user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: now.AddMinutes(2),
+            signingCredentials: credentials);
+
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ HongPet.Backend/src/HongPet.WebApi/ServiceEnxtensions/AuthenticationConfig.cs
@@ -0,0 +1,28 @@
+﻿using HongPet.Application.Commons;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HongPet.WebApi.ServiceEnxtensions;
+
+public static class AuthenticationConfig
+{
+    public static IServiceCollection AddJwtConfiguration
+        (this IServiceCollection services, AppConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(config.JwtConfiguration.SecretKey))
+                    };
+                });
+        return services;
+    }
+}
