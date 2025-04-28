6863e1823f138716963b783f3dab9e315322493f
vliam0602 <v.trclam@gmail.com>
2025-04-29 01:14:19 +0700

    add customer view order history

---
+++ HongPet.CustomerSite/HongPet.CustomerMVC/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HongPet.CustomerMVC.Controllers;
+public class UserController : Controller
+{
+    public IActionResult OrderHistory()
+    {
+        return View();
+    }
+}
